744538d914d33f5b083a5d81fffc3afab48356f9
Mohammad Askar <m.askar@isecur1ty.org>
2017-01-06 17:58:08 +0200

    Update AES_encryption.py

--- AES_encryption.py
+++ AES_encryption.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python
-from Crypto.Cipher import AES
-from termcolor import cprint
-from Crypto.Util import Counter
+from Crypto.Cipher import AES # import AES Chiper Algorithm
+from termcolor import cprint #import cprint to print text with colors
+from Crypto.Util import Counter #import Counter to use in CTR AES mode.
 file_to_save = raw_input("[~]Please enter file name to save the cipher text >> ") #file to save the ciphertext
 key = raw_input("[~]Please Enter the key (16 or 24 or 32 byte) >> ") #KEY
 ctr=Counter.new(128) #counter length
 mode = AES.MODE_CTR #Counter Mode
-encryptor = AES.new(key, mode,counter=ctr) #Create new AES object 
+encryptor = AES.new(key, mode,counter=ctr) #Create a new AES cipher 
 text = raw_input("[~]Please Enter your text >> ") #TEXT
 ciphertext = encryptor.encrypt(text) #encrypt the data
 try:
- f = open(file_to_save,"w")
- f.write(ciphertext)
- f.close()
+ f = open(file_to_save,"w") #open file to save
+ f.write(ciphertext) # write chiper text to this file
+ f.close() # close file
  cprint("[+]Chiper Text is saved to file {0}".format(file_to_save),"green")
 except:
  cprint("[!]Error while writing to {0}".format(file_to_save),"red")
